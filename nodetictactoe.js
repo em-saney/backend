8ca35121b71150f8eaf0d8944416aab50322aefb
Sani Ismail <39333510+em-saney@users.noreply.github.com>
2023-10-23 08:59:31 +0100

    Update nodetictactoe.js

--- nodetictactoe.js
+++ nodetictactoe.js
@@ -14,20 +14,19 @@ let board = [
 let currentPlayer = 'X';
 
 function printBoard() {
+  
   for (let i = 0; i < 3; i++) {
     console.log(` ${i + 1} ${board[i].join(' | ')}`);
-    console.log(' -----------');
+    console.log(' -----------'); //seperator
   }
 }
 
 function checkWin() {
-  // Check rows, columns, and diagonals
-  // ...
 }
 
 function gameLoop() {
   printBoard();
-  rl.question(`Player ${currentPlayer}, enter your move (row and column): `, answer => {
+  rl.question(`Player ${currentPlayer}, Turn (row and column): `, answer => {
     const [row, col] = answer.split(' ').map(num => parseInt(num) - 1);
 
     if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] === ' ') {
@@ -35,14 +34,16 @@ function gameLoop() {
 
       if (checkWin()) {
         printBoard();
-        console.log(`Player ${currentPlayer} wins! Congratulations!`);
+        console.log(`Player ${currentPlayer} wins`);
         rl.close();
-      } 
-      else {
+      } else {
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         gameLoop();
       }
-    } 
+    } else {
+      console.log('Invalid');
+      gameLoop();
+    }
   });
 }
 
